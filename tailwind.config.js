15e9b91a7d53e1804baee806f17844932b4fdecf
iobra <iobradovuch@mgail.com>
2024-06-06 13:13:15 +0300

    3-thymeleaf-with-tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme= require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./src/main/resources/templates/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'demo-green': 'darkseagreen'
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      plugins: [
+        require('@tailwindcss/container-queries'),
+        require('daisyui'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography')
+      ],
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/container-queries'),require('daisyui'),require('@tailwindcss/forms'),require('@tailwindcss/typography')],
 }
-
