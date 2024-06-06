0007be094f8724e05500b6024c463dd2ca611a42
iobra <iobradovuch@mgail.com>
2024-06-06 14:34:57 +0300

    5- Added separate controllers for a few sidebar menu item

--- src/main/java/com/example/demo/dashboard/DashboardController.java
+++ src/main/java/com/example/demo/dashboard/DashboardController.java
@@ -1,14 +1,16 @@
-package com.example.demo;
+package com.example.demo.dashboard;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-public class HomeController {
-    @GetMapping("/")
-    public String index() {
-        return "index"; // This corresponds to index.html in the templates folder
+public class DashboardController {
+
+    @GetMapping
+    public String projects(Model model) {
+        return "dashboard/dashboard";
     }
 }
