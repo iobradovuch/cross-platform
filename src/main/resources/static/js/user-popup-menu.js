9449f1e15fb600f4046d196efcf2c6069def78c4
iobra <iobradovuch@mgail.com>
2024-06-06 14:20:23 +0300

    4- Refactored the structure of app templates by adding common layout

--- src/main/resources/static/js/user-popup-menu.js
+++ src/main/resources/static/js/user-popup-menu.js
@@ -9,6 +9,14 @@ function userPopupMenu() {
         },
         isVisible() {
             return this.show === true;
+        },
+        highlightMenuItem(event) {
+            event.target.classList.add('font-bold'); // Додаємо жирний стиль
+            event.target.classList.add('bg-gray-200'); // Змінюємо фон на темніший
+        },
+        resetMenuItem(event) {
+            event.target.classList.remove('font-bold'); // Видаляємо жирний стиль
+            event.target.classList.remove('bg-gray-200'); // Відновлюємо оригінальний фон
         }
     };
 }
